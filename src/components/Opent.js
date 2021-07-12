e0143bdbcba19c239727b6c5b02c5608642655f2
Srikanth Malle <smalle@adaequare.com>
2021-07-12 16:08:53 +0530

    sv

---
+++ src/components/Opent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class Opent extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                <>
+                <h1>Shivani</h1>
+                </>
+            </div>
+        )
+    }
+}
+
+
+export default Opent
+
